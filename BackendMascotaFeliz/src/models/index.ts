2a1f3ebc50f11be37f8049162334a83454002178
Hugo Paz <hugo.a.paz7@gmail.com>
2022-10-30 23:43:56 -0500

    V1

---
+++ BackendMascotaFeliz/src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './usuario.model';
+export * from './mascota.model';
+export * from './plan.model';
+export * from './sucursal.model';
+export * from './productos-servicio.model';
+export * from './prospecto.model';
